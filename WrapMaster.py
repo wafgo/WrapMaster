7f14a84c81026c16d456d05ee7ef7254d0000441
Wadim Mueller <wafgo01@gmail.com>
2024-04-10 17:03:23 +0200

    fix also symbols and not only relocations

--- WrapMaster.py
+++ WrapMaster.py
@@ -1,16 +1,19 @@
 # Author: Wadim Mueller <wafgo01@gmail.com>
-
-import lief
-import argparse
-import os
-import logging
 import sys
+import os
+try:
+    import lief
+except ImportError:
+    print('LIEF is not installed. Either install it via "pip install lief", or use the virtualenv via "source env/activate.sh"')
+    sys.exit(1)
+import argparse
+import logging
 
 logging.basicConfig(level=logging.WARNING)
 
 script_version_major = "0"
-script_version_minor = "1"
-script_version_patch = "1"
+script_version_minor = "2"
+script_version_patch = "0"
 
 script_version = 'v' + script_version_major + '.' + script_version_minor + '.' + script_version_patch
 
@@ -33,7 +36,12 @@ def add_real_symbol(obj, symbol):
     """
     for the compilation unit where the "to be wrapped" function is defined we need to create a symbol with the name
     __real_XX so that the wrapper function (__wrap_XX) can call the real implementation via a call to real_XX.
+    Only create the symbol if it is not already available in this CU, otherwise multiple defined linker errors will occur
     """
+    for sym in obj.symbols:
+        if sym.name == '__real_' + symbol.name:
+            return
+        
     real_sym = lief.ELF.Symbol()
     real_sym.name = "__real_" + symbol.name
     real_sym.binding = symbol.binding
@@ -49,6 +57,36 @@ def add_real_symbol(obj, symbol):
     obj.add_static_symbol(real_sym)
 
 
+def fix_symbols(obj, obj_file_name, fname):
+    for idx, sym in enumerate(obj.symbols):
+        if not sym.name.strip():
+            continue
+        if sym.name == fname:
+            if sym.shndx != lief.ELF.SYMBOL_SECTION_INDEX.UNDEF:
+                add_real_symbol(obj, sym)
+            else:
+                sym.name = "__wrap_" + fname
+        
+def fix_relocations(obj, obj_file_name, fname):
+    """
+    
+    """
+    for idx, reloc in enumerate(obj.relocations):
+        logging.debug(f"{idx}/{len(obj.relocations)}")
+        if not reloc.symbol.name.strip():
+            continue
+        logging.debug(f"{reloc.symbol.name} -> {fname}")
+        if reloc.symbol.name == fname:
+            logging.debug(f"Found {fname} in {obj_file_name}")
+            if reloc.symbol.shndx != lief.ELF.SYMBOL_SECTION_INDEX.UNDEF:
+                logging.debug(f"{fname} is defined in {obj_file_name}")
+                add_real_symbol(obj, reloc.symbol)
+                reloc.symbol = file_obj.add_static_symbol(make_undef_wrap_symbol(fname))
+            else:
+                logging.debug(f"{fname} is not defined in {obj_file_name}")
+                reloc.symbol.name = "__wrap_" + reloc.symbol.name
+                        
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Dynamic Function Wrapping for Enhanced Debugging", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-c", "--config_file", required=False,
@@ -67,7 +105,6 @@ if __name__ == '__main__':
         sys.exit(0)
 
     args = parser.parse_args()
-
         
     if args.config_file != '':
         if not os.path.isfile(args.config_file):
@@ -92,13 +129,7 @@ if __name__ == '__main__':
         file_obj = lief.parse(obj_file)
         for wrap_func in args.wrap:
             logging.debug(f"Processing {wrap_func} in {obj_file} ...")
-            for reloc in file_obj.relocations:
-                logging.debug(reloc)
-                if reloc.symbol.name == wrap_func:
-                    if reloc.symbol.shndx != lief.ELF.SYMBOL_SECTION_INDEX.UNDEF:
-                        add_real_symbol(file_obj, reloc.symbol)
-                        reloc.symbol = file_obj.add_static_symbol(make_undef_wrap_symbol(wrap_func))
-                    else:
-                        reloc.symbol.name = "__wrap_" + reloc.symbol.name
+            fix_symbols(file_obj, obj_file, wrap_func)
+            fix_relocations(file_obj, obj_file, wrap_func)
                         
         file_obj.write(obj_file)
